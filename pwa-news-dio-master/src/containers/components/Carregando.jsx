699afe47eb9866ac54fddd1fda1433502c4e4225
fabioOUO <fabiojbarbosa1@gmail.com>
2021-09-27 21:05:05 -0300

    Add files

---
+++ pwa-news-dio-master/src/containers/components/Carregando.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../style.css'
+export default function Carregando() {
+    return (
+        <div className="loading">
+            <header className="title">
+                Carregando...
+            </header>
+        </div>
+    )
+}
